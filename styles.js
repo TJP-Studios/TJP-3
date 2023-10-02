79da574e4bf512db6b01798eba6bcf32d982c669
Obyno003 <ogbonnaemmanuel003@gmail.com>
2023-10-02 11:07:02 +0100

    Added some changes

--- styles.js
+++ styles.js
@@ -1,5 +1,13 @@
-var im = document.getElementById('.test');
+// var im = document.getElementById('.test');
 
-img.hover = function(){
-    im.style.display = "block";
-}
+// img.hover = function(){
+//     im.style.display = "block";
+// }
+
+// function nig(x) {
+//     x.style.color = "blue";
+// } 
+
+// function niga(x) {
+//     x.style.color = "white";
+// } 
